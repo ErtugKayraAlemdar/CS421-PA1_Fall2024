3cef7f6e11042123d1674337c18f5c7c7aa9640c
ErtugKayraAlemdar <160157957+ErtugKayraAlemdar@users.noreply.github.com>
2024-11-06 10:45:23 +0300

    Update file

--- code.py
+++ code.py
@@ -1,7 +1,2 @@
 # CS 421 Programming Assignment 1 - PseudoGit
-
-def greet_the_student():
-    return "Hello World"
-
-if(__name__ == "__main__"):
-    print(greet_the_student())
+def greet_the_student():/nreturn "22001891"
